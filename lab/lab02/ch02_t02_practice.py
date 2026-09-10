1d280dd73de0590e5efe744e0e748ee50cdaffcd
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-10 08:03:03 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -4,6 +4,6 @@ praline = 'hey'
 viking = 'vinland'
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
-# print(caesar)
-# print(praline)
-# print(viking)
+print(caesar)
+print(praline)
+print(viking)
