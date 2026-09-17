a6ca187ddc7aa078aaba3a3a2b2811e32db3e441
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 12:22:20 +0000

    lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -1,6 +1,7 @@
 def hotel_cost(nights: int) -> int:
     return 140 * nights
 
+
 def plane_ride_cost(city):
     if city == "Charlotte":
         return 183
@@ -8,4 +9,5 @@ def plane_ride_cost(city):
         return 220
     elif city == 'Pittsburgh':
         return 222
-    elif city == ''
+    elif city == 'Los Angeles':
+        return 475
