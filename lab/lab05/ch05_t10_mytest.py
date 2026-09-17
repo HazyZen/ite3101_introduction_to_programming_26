72c674d7db01fbc6897397213c49e3f4d1386a0f
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:01:35 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -6,6 +6,6 @@ if len(original) > 0 and original.isalpha():
     word = original.lower() #all lowercase
     first = word[0] #gets the first letter of the word
     new_word = word + first + pyg 
-    new_word = new_word[1:len(new_word)] #
+    new_word = new_word[1:len(new_word)] #slices out the first letter and gives the rest
 else:
     print('empty')
