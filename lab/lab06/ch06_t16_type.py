13380744fe022ff81a6f83377c665646fc1dc441
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:43:08 +0000

    lab/lab06/ch06_t16_type.py

--- lab/lab06/ch06_t16_type.py
+++ lab/lab06/ch06_t16_type.py
@@ -1,5 +1,5 @@
 # Print out the types of an integer, a float,
 # and a string on separate lines below.
-print(type('hello world'))
-print(type())
-print(type())
+print(type(11))
+print(type(3.17))
+print(type('hello'))
