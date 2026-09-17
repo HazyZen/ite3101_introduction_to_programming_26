d882329443d6ebbf26ed5612a16d207e6fc44b6c
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 12:32:56 +0000

    [skip actions] lab/lab07/ch07_t06_never_know.py

--- lab/lab07/ch07_t06_never_know.py
+++ lab/lab07/ch07_t06_never_know.py
@@ -24,4 +24,4 @@ def rental_car_cost(days: int) -> int:
 
 def trip_cost(city: str, days: int, spending_money: int) -> int:
     spending_money = 0
-    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
+    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money
