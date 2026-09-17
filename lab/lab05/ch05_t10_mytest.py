6417f66dd58189b05be57889ba9ceb22c791d0e3
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:01:52 +0000

    lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -3,9 +3,11 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = original.lower() #all lowercase
-    first = word[0] #gets the first letter of the word
-    new_word = word + first + pyg 
-    new_word = new_word[1:len(new_word)] #slices out the first letter and gives the rest
+    word = original.lower()  # all lowercase
+    first = word[0]  # gets the first letter of the word
+    new_word = word + first + pyg
+    # slices out the first letter and gives the rest
+    new_word = new_word[1:len(new_word)]
+    print(new_word)
 else:
     print('empty')
