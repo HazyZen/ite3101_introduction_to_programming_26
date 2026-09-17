a451d711ddc462127475c55740c0c2f494cc2bd1
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 07:34:40 +0000

    lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -4,9 +4,9 @@ def grade_converter(grade: int) -> str:
         return "A"
     elif grade >= 80 and grade <= 89:
         return "B"
-    elif grade >= 80 and grade <= 89:
+    elif grade >= 70 and grade <= 79:
         return "C"
-    elif None:
+    elif grade >= 80 and grade <= 89:
         return "D"
     else:
         return "F"
