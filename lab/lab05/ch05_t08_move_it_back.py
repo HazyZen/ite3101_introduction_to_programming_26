6bd969405d26196991e19831f8d35ca7f0ff30b5
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 07:57:52 +0000

    lab/lab05/ch05_t08_move_it_back.py

--- lab/lab05/ch05_t08_move_it_back.py
+++ lab/lab05/ch05_t08_move_it_back.py
@@ -4,7 +4,7 @@ original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     word = original.lower()
-    new_word = 
     first = word[0]
+    new_word = word + first + pyg
 else:
     print("empty")
