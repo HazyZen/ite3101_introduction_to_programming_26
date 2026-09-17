685809092106bedf28cb8f3b50105f3673c32cd3
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:14:44 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,8 +1,9 @@
 def cube(number):
     return number ** number
 
+
 def by_three(number):
-    if number / 3 == 0:
+    if (number / 3 == 0):
         return cube(number)
     else:
         return False
