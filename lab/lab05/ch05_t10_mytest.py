aecf6cc17aacf7fc3e54b2357fc7530e4ea3703e
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:00:39 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -3,7 +3,7 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = original.lower()
+    word = original.lower() #
     first = word[0]
     new_word = word + first + pyg
     new_word = new_word[1:len(new_word)]
