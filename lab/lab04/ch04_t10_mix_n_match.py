8fa4e128579ce74ec5ef57682d9c326240f3c999
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 07:23:09 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = not False or (6 != 6)
 
 # Make me false!
-bool_three = None
+bool_three = 5 == 5 
 
 # Make me true!
 bool_four = None
