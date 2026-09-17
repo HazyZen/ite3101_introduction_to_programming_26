715d31df31790885b98846620284cdd0d036dde6
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-17 08:01:13 +0000

    [skip actions] lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -5,7 +5,7 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower() #all lowercase
     first = word[0] #gets the first letter of the word
-    new_word = word + first + pyg #
-    new_word = new_word[1:len(new_word)]
+    new_word = word + first + pyg 
+    new_word = new_word[1:len(new_word)] #
 else:
     print('empty')
