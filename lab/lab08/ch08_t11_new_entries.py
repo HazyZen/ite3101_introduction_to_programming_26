33c14c4cb216ee90f9683da7942470ac78106573
Waqas <115215102+HazyZen@users.noreply.github.com>
2026-09-24 07:32:34 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -4,6 +4,7 @@ print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
 menu['Sushi'] = 18
+menu['Steak'] = 29.90
 menu['']
 
 print("There are " + str(len(menu)) + " items on the menu.")
